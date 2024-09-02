2967db74bb4de576ed91dc3aaafa6f9ee2d85dbb
Otavio Felipe dos Santos <169068279+otavio444@users.noreply.github.com>
2024-09-02 23:12:58 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int dia = 22;
+    int mes = 06;
+    int ano = 2009;
+    double pi = 3.141592;
+    System.out.printf(" o valor de pi é: %.2f",pi);
+   
+  }
+
+  
+}
